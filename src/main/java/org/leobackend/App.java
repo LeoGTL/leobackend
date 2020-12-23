2f89083ed923c78edb33af3152fdc2c63be7981f
leoGTL <leogtl@163.com>
2020-12-23 23:18:35 +0800

    构建项目骨架

---
+++ src/main/java/org/leobackend/App.java
@@ -0,0 +1,21 @@
+package org.leobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2020-12-23.
+ * 启动类而已
+ *
+ * @author Leo
+ */
+@SpringBootApplication
+@SpringBootConfiguration
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
