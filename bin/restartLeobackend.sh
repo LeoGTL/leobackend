34d87cbf45d343208b77ee3348892a505292b4c4
Leo <leogtl@163.com>
2020-12-24 14:19:14 +0800

    新增启动脚本

---
+++ bin/restartLeobackend.sh
@@ -0,0 +1,16 @@
+#!/usr/bin bash
+
+
+echo "停止正在运行的程序"
+leobackendpid=$(ps -aux | grep leobackend | grep -v grep | awk '{print $2}')
+if [ -n "$leobackendpid" ];
+then
+    kill -9 "$leobackendpid"
+fi
+
+sleep 3
+
+echo "启动程序"
+nohup $JAVA_HOME/bin/java -jar target/leobackend-1.0-SNAPSHOT.jar >> logs/leobackend.log &
+
+exit 0
