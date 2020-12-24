1003a0ab60e4cdceb57c303dafeed010a6fa9ee9
Leo <leogtl@163.com>
2020-12-24 14:27:30 +0800

    脚本更新

--- bin/restartLeobackend.sh
+++ bin/restartLeobackend.sh
@@ -10,6 +10,6 @@ fi
 sleep 3
 
 echo "##重新启动程序"
-nohup $JAVA_HOME/bin/java -jar ../target/leobackend-1.0-SNAPSHOT.jar >> ../logs/leobackend.log &
+nohup $JAVA_HOME/bin/java -jar target/leobackend-1.0-SNAPSHOT.jar >> logs/leobackend.log &
 
 exit 0
