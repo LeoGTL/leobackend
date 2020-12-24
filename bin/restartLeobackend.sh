adb7d1ebbc2c6741e2342d22eec79c7bbcdb531c
Leo <leogtl@163.com>
2020-12-24 14:26:22 +0800

    脚本更新

--- bin/restartLeobackend.sh
+++ bin/restartLeobackend.sh
@@ -1,7 +1,6 @@
 #!/usr/bin bash
 
-
-echo "停止正在运行的程序"
+echo "##停止正在运行的程序..."
 leobackendpid=$(ps -aux | grep leobackend | grep -v grep | awk '{print $2}')
 if [ -n "$leobackendpid" ];
 then
@@ -10,7 +9,7 @@ fi
 
 sleep 3
 
-echo "启动程序"
-nohup $JAVA_HOME/bin/java -jar target/leobackend-1.0-SNAPSHOT.jar >> logs/leobackend.log &
+echo "##重新启动程序"
+nohup $JAVA_HOME/bin/java -jar ../target/leobackend-1.0-SNAPSHOT.jar >> ../logs/leobackend.log &
 
 exit 0
